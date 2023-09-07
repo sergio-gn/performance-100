4ee5c37c2dce978a49f2cfadfc9b900bce559804
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-09-07 21:13:20 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector('.burger i');
+const nav = document.querySelector('.nav');
+function toggleNav() {
+  burger.classList.toggle('fa-bars');
+  burger.classList.toggle('fa-times');
+  nav.classList.toggle('nav-active');
+  var x = document.getElementById("nav");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
+burger.addEventListener('click', function () {
+  toggleNav();
+});
